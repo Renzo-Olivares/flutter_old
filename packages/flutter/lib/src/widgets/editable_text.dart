d47f9d73b2a694e20d9dd4e1f91a3d0e2184d8ac
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-08-09 12:58:56 -0700

    updates

--- packages/flutter/lib/src/widgets/editable_text.dart
+++ packages/flutter/lib/src/widgets/editable_text.dart
@@ -136,6 +136,230 @@ class TextEditingInlineSpanReplacement {
   }
 }
 
+class InlineSpanReplacementManager {
+  InlineSpanReplacementManager();
+
+  List<TextEditingInlineSpanReplacement> replacements = [];
+  List<int> replacementStartingPositions = [];
+  List<int> replacementEndingPositions = [];
+  List<int> replacementMaxes = [];
+  List<int> replacementOrder = [];
+  Map<TextEditingInlineSpanReplacement, int> indexOfReplacements = {};
+  int replacementInsertCount = 0;
+  int replacementCount = 0;
+  int lowWaterMark = 0; // indices below this have not been touched. maybe make this nullable
+  int gapStart = 0; //init to length of string
+  int gapLength = 0; //end - start
+
+  // Primitives for treating span list as binary tree
+
+  // The spans (along with start and end offsets and flags) are stored in linear arrays sorted
+  // by start offset. For fast searching, there is a binary search structure imposed over these
+  // arrays. This structure is inorder traversal of a perfect binary tree, a slightly unusual
+  // but advantageous approach.
+
+  // The value-containing nodes are indexed 0 <= i < n (where n = mSpanCount), thus preserving
+  // logic that accesses the values as a contiguous array. Other balanced binary tree approaches
+  // (such as a complete binary tree) would require some shuffling of node indices.
+
+  // Basic properties of this structure: For a perfect binary tree of height m:
+  // The tree has 2^(m+1) - 1 total nodes.
+  // The root of the tree has index 2^m - 1.
+  // All leaf nodes have even index, all interior nodes odd.
+  // The height of a node of index i is the number of trailing ones in i's binary representation.
+  // The left child of a node i of height h is i - 2^(h - 1).
+  // The right child of a node i of height h is i + 2^(h - 1).
+
+  // Note that for arbitrary n, interior nodes of this tree may be >= n. Thus, the general
+  // structure of a recursive traversal of node i is:
+  // * traverse left child if i is an interior node
+  // * process i if i < n
+  // * traverse right child if i is an interior node and i < n
+  int _treeRoot() {
+    return _highestOneBit(replacementCount) - 1;
+  }
+
+  // TODO: https://pub.dev/documentation/scidart/latest/numdart/highestOneBit.html
+  int _highestOneBit(int n) {
+    n |= (n >> 1);
+    n |= (n >> 2);
+    n |= (n >> 4);
+    n |= (n >> 8);
+    n |= (n >> 16);
+    return n - (n >> 1);
+  }
+
+  // (i+1) & ~i is equal to 2^(the number of trailing ones in i)
+  static int _leftChild(int i) {
+    return i - (((i + 1) & ~i) >> 1);
+  }
+
+  static int _rightChild(int i) {
+    return i + (((i + 1) & ~i) >> 1);
+  }
+
+  // The span arrays are also augmented by an mSpanMax[] array that represents an interval tree
+  // over the binary tree structure described above. For each node, the mSpanMax[] array contains
+  // the maximum value of mSpanEnds of that node and its descendants. Thus, traversals can
+  // easily reject subtrees that contain no spans overlapping the area of interest.
+
+  // Note that mSpanMax[] also has a valid valuefor interior nodes of index >= n, but which have
+  // descendants of index < n. In these cases, it simply represents the maximum span end of its
+  // descendants. This is a consequence of the perfect binary tree structure.
+  int _calcMax(int i) {
+    int max = 0;
+    if ((i & 1) != 0) {
+      // internal tree node
+      max = _calcMax(_leftChild(i));
+    }
+    if (i < replacementCount) {
+      max = math.max(max, replacementEndingPositions[i]);
+      if ((i & 1) != 0) {
+        max = math.max(max, _calcMax(_rightChild(i)));
+      }
+    }
+    replacementMaxes[i] = max;
+    return max;
+  }
+
+  // restores binary interval tree invariants after any mutation of span structure
+  void _restoreInvariants() {
+    if (replacementCount == 0) return;
+
+    // invariant 1: span starts are nondecreasing
+
+    // This is a simple insertion sort because we expect it to be mostly sorted.
+    for (int i = 1; i < replacementCount; i++) {
+      if (replacementStartingPositions[i] < replacementEndingPositions[i - 1]) {
+        TextEditingInlineSpanReplacement replacement = replacements[i];
+        int start = replacementStartingPositions[i];
+        int end = replacementEndingPositions[i];
+        // int flags = replacementFlags[i];
+        int insertionOrder = replacementOrder[i];
+        int j = i;
+        do {
+          replacements[j] = replacements[j - 1];
+          replacementStartingPositions[j] = replacementStartingPositions[j - 1];
+          replacementEndingPositions[j] = replacementEndingPositions[j - 1];
+          // replacementFlags[j] = replacementFlags[j - 1];
+          replacementOrder[j] = replacementOrder[j - 1];
+          j--;
+        } while (j > 0 && start < replacementStartingPositions[j - 1]);
+        replacements[j] = replacement;
+        replacementStartingPositions[j] = start;
+        replacementEndingPositions[j] = end;
+        // replacementFlags[j] = flags;
+        replacementOrder[j] = insertionOrder;
+        _invalidateIndex(j);
+      }
+    }
+
+    // invariant 2: max is max span end for each node and its descendants
+    _calcMax(_treeRoot());
+
+    // invariant 3: mIndexOfSpan maps spans back to indices
+    if (indexOfReplacements == null) {
+      indexOfReplacements = <TextEditingInlineSpanReplacement, int>{};
+    }
+    for (int i = lowWaterMark; i < replacementCount; i++) {
+      int? existing = indexOfReplacements[replacements[i]];
+      if (existing == null || existing != i) {
+        indexOfReplacements[replacements[i]] = i;
+      }
+    }
+    final int minInt = (double.infinity is int) ? -double.infinity as int : (-1 << 63);
+    final int maxInt = (double.infinity is int) ? double.infinity as int : ~minInt;
+
+    lowWaterMark = maxInt;
+  }
+
+  // Call this on any update to mSpans[], so that mIndexOfSpan can be updated
+  void _invalidateIndex(int i) {
+    lowWaterMark = math.min(i, lowWaterMark);
+  }
+
+  void setReplacement(TextEditingInlineSpanReplacement replacement) {
+    int start = replacement.range.start;
+    int end = replacement.range.end;
+    int nstart = replacement.range.start;
+    int nend = replacement.range.end;
+
+    if (start > gapStart) {
+      start += gapLength;
+    } else if (start == gapStart) {
+      // if (flagsStart == POINT || (flagsStart == PARAGRAPH && start == length()))
+      start += gapLength;
+    }
+
+    if (end > gapStart) {
+      end += gapLength;
+    } else if (end == gapStart) {
+      // if (flagsEnd == POINT || (flagsEnd == PARAGRAPH && end == length()))
+      end += gapLength;
+    }
+
+    if (indexOfReplacements != null) {
+      int? index = indexOfReplacements[replacement];
+      if (index != null) {
+        int i = index;
+        int ostart = replacementStartingPositions[i];
+        int oend = replacementEndingPositions[i];
+
+        if (ostart > gapStart)
+          ostart -= gapLength;
+        if (oend > gapStart)
+          oend -= gapLength;
+
+        replacementStartingPositions[i] = start;
+        replacementEndingPositions[i] = end;
+        // replacementFlags[i] = flags;
+
+        // if (send) {
+        if (true) {
+          _restoreInvariants();
+          // sendSpanChanged(what, ostart, oend, nstart, nend);
+        }
+
+        return;
+      }
+    }
+
+    replacements.add(replacement);
+    replacementStartingPositions.add(start);
+    replacementEndingPositions.add(end);
+    // replacementFlags.add(flags);
+    replacementOrder.add(replacementInsertCount);
+    _invalidateIndex(replacementCount);
+    replacementCount++;
+    replacementInsertCount++;
+    // Make sure there is enough room for empty interior nodes.
+    // This magic formula computes the size of the smallest perfect binary
+    // tree no smaller than mSpanCount.
+    int sizeOfMax = 2 * _treeRoot() + 1;
+    if (replacementMaxes.length < sizeOfMax) {
+      replacementMaxes = new List<int>.filled(sizeOfMax, -1, growable: true);
+      // replacementMaxes = [];
+    }
+
+    print(replacements);
+    print(replacementStartingPositions);
+    print(replacementEndingPositions);
+    print(replacementOrder);
+    print(replacementCount);
+    print(replacementInsertCount);
+
+    // if (send) {
+    if (true) {
+      _restoreInvariants();
+      // sendSpanAdded(what, nstart, nend);
+    }
+  }
+
+  void adjustReplacementRanges(TextDelta textDelta) {
+
+  }
+}
+
 /// A controller for an editable text field.
 ///
 /// Whenever the user modifies a text field with an associated
@@ -391,6 +615,7 @@ class ReplacementTextEditingController extends TextEditingController {
   ///
   /// TODO: Give an example of replacements matching against the same range of text.
   List<TextEditingInlineSpanReplacement>? replacements;
+  InlineSpanReplacementManager? replacementManager;
 
   /// If composing regions should be matched against for replacements.
   ///
@@ -403,12 +628,7 @@ class ReplacementTextEditingController extends TextEditingController {
   final bool composingRegionReplaceable;
 
   void applyReplacement(TextEditingInlineSpanReplacement replacement) {
-    if (replacements == null) {
-      replacements = [];
-      replacements!.add(replacement);
-    } else {
-      replacements!.add(replacement);
-    }
+    replacementManager.setReplacement(replacement);
   }
 
   /// Update replacement ranges based on information sent from a supplementary
@@ -502,6 +722,18 @@ class ReplacementTextEditingController extends TextEditingController {
       replacements!.clear();
       replacements!.addAll(updatedReplacements);
     }
+  }
+
+  @override
+  set value(TextEditingValue newValue) {
+    assert(
+    !newValue.composing.isValid || newValue.isComposingRangeValid,
+    'New TextEditingValue $newValue has an invalid non-empty composing range '
+        '${newValue.composing}. It is recommended to use a valid composing range, '
+        'even for readonly text fields',
+    );
+    replacementManager.adjustReplacementRanges(newValue.delta);
+    super.value = newValue;
   }
 
   @override
