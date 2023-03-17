37c64e3d1ffd1eb72d53cba6d011d7645e3c054c
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-03-17 15:57:59 -0700

    Revert "Revert "naive attempt at stopping overlay from absorbing all hit testing""
    This reverts commit 28144d201abd1c9061f6885103e8e5d4d2332d76.

--- packages/flutter/test/material/text_field_test.dart
+++ packages/flutter/test/material/text_field_test.dart
@@ -9132,7 +9132,6 @@ void main() {
     testWidgets(
       'Can triple tap to select a paragraph on mobile platforms when tapping at a word edge',
       (WidgetTester tester) async {
-        // TODO(Renzo-Olivares): Enable for iOS, currently broken because selection overlay blocks the tapanddraggesturerecognizer.
         final TextEditingController controller = TextEditingController();
         final bool isTargetPlatformApple = defaultTargetPlatform == TargetPlatform.iOS;
 
@@ -9183,7 +9182,7 @@ void main() {
         expect(controller.selection.baseOffset, 0);
         expect(controller.selection.extentOffset, 20);
       },
-      variant: const TargetPlatformVariant(<TargetPlatform>{ TargetPlatform.android, TargetPlatform.fuchsia }),
+      variant: const TargetPlatformVariant(<TargetPlatform>{ TargetPlatform.android, TargetPlatform.fuchsia, TargetPlatform.iOS }),
     );
 
     testWidgets(
