1835437df7f2243b0508a58c076e33be5b19e7ac
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-04-18 11:17:09 -0700

    Add test to move selection back to origin word when selecting across multiple widgets - failing currently

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -1447,7 +1447,6 @@ class _SelectableFragment with Selectable, ChangeNotifier implements TextLayoutM
   }
 
   SelectionResult _updateSelectionEdge(Offset globalPosition, {required bool isEnd}) {
-    // debugPrint('${this.hashCode}');
     _setSelectionPosition(null, isEnd: isEnd);
     final Matrix4 transform = paragraph.getTransformTo(null);
     transform.invert();
@@ -1477,7 +1476,6 @@ class _SelectableFragment with Selectable, ChangeNotifier implements TextLayoutM
   }
 
   SelectionResult _updateSelectionEdgeByWord(Offset globalPosition, {required bool isEnd}) {
-    // debugPrint('${this.hashCode}');
     _setSelectionPosition(null, isEnd: isEnd);
     final Matrix4 transform = paragraph.getTransformTo(null);
     transform.invert();
