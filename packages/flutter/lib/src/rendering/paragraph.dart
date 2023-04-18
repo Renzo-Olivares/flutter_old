8035ad90f67d62aed6843f7b75343857f2686649
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-04-18 10:28:34 -0700

    Add some more tests and tweak _shouldMoveEndEdge

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -1445,6 +1445,7 @@ class _SelectableFragment with Selectable, ChangeNotifier implements TextLayoutM
   }
 
   SelectionResult _updateSelectionEdge(Offset globalPosition, {required bool isEnd}) {
+    // debugPrint('${this.hashCode}');
     _setSelectionPosition(null, isEnd: isEnd);
     final Matrix4 transform = paragraph.getTransformTo(null);
     transform.invert();
@@ -1474,6 +1475,7 @@ class _SelectableFragment with Selectable, ChangeNotifier implements TextLayoutM
   }
 
   SelectionResult _updateSelectionEdgeByWord(Offset globalPosition, {required bool isEnd}) {
+    // debugPrint('${this.hashCode}');
     _setSelectionPosition(null, isEnd: isEnd);
     final Matrix4 transform = paragraph.getTransformTo(null);
     transform.invert();
