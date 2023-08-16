62687f95c31a3c83e27b67aef184cb396a6ae42e
Renzo Olivares <rmolivares@renzo-olivares.dev>
2023-08-16 13:01:22 -0700

    Paragraph.selection should not return empty for collapsed selections

--- packages/flutter/lib/src/rendering/paragraph.dart
+++ packages/flutter/lib/src/rendering/paragraph.dart
@@ -354,8 +354,7 @@ class RenderParagraph extends RenderBox with ContainerRenderObjectMixin<RenderBo
     final List<TextSelection> results = <TextSelection>[];
     for (final _SelectableFragment fragment in _lastSelectableFragments!) {
       if (fragment._textSelectionStart != null &&
-          fragment._textSelectionEnd != null &&
-          fragment._textSelectionStart!.offset != fragment._textSelectionEnd!.offset) {
+          fragment._textSelectionEnd != null) {
         results.add(
           TextSelection(
             baseOffset: fragment._textSelectionStart!.offset,
